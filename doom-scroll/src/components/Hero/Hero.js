dadcfd258b5c0b1f6049010f6c23834ca24c331e
Duy Pham <duypham12241999@gmail.com>
2022-06-15 19:40:52 -0700

    Change definition to blockquote

--- doom-scroll/src/components/Hero/Hero.js
+++ doom-scroll/src/components/Hero/Hero.js
@@ -2,19 +2,15 @@ export const Hero = () => {
   return (
     <div className="h-screen bg-cyan-400">
       <h3>
-        Doomscrolling and doomsurfing are new terms referring to the tendency to
-        continue to surf or scroll through bad news, even though that news is
-        saddening, disheartening, or depressing. --www.merriam-webster.com
+        <blockquote>
+          Doomscrolling and doomsurfing are new terms referring to the tendency
+          to continue to surf or scroll through bad news, even though that news
+          is saddening, disheartening, or depressing. --www.merriam-webster.com
+        </blockquote>
       </h3>
-      <h1>
-        Doom Scroll
-      </h1>
-      <h2>
-        See the Best and Worst of the Internet
-      </h2>
-      <p>
-        Designed and Built by Duy Pham
-      </p>
+      <h1>Doom Scroll</h1>
+      <h2>See the Best and Worst of the Internet</h2>
+      <p>Designed and Built by Duy Pham</p>
     </div>
   );
 };
