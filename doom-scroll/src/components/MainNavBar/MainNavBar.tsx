be690ceb2368c08a64054c8956b0fb872f1ac7db
Duy Pham <duypham12241999@gmail.com>
2022-06-30 23:15:58 -0700

    Make nav bar more responsive

--- doom-scroll/src/components/MainNavBar/MainNavBar.tsx
+++ doom-scroll/src/components/MainNavBar/MainNavBar.tsx
@@ -15,7 +15,7 @@ const MAIN_NAV_BAR_PATHS = {
 };
 
 const MainNavBar: FC<Props> = () => {
-    return <SlideNavBar navBarPaths={MAIN_NAV_BAR_PATHS} bottomMargin={500} />;
+    return <SlideNavBar navBarPaths={MAIN_NAV_BAR_PATHS} bottomMargin={0} />;
 };
 
 export default MainNavBar;
